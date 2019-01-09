6543d1b96fa205a7d9c27bb2c1f54f410488330d
Sorawit Lertcharernrit <sorawit.lertcharernrit@gmail.com>
2019-01-09 15:49:52 +0700

    Change to my access_token

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '2lCdm/gOh0aY91WBYgSIElqBhaBdO5JQ4TGsEaMAXg1dIK2+JNajDLdHslTpoVYpNsEcAibd6dS9qCVYgsKSNAIlb0SpjOx/il1l/5ieloAM/ornHV78rqCeLnaG2tgyXyOjk43pbpeJu9IhA3ucawdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
