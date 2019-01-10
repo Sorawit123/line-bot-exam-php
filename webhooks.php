d8766572a732086f2061d8fe53eef2aa3e688e79
Sorawit Lertcharernrit <sorawit.lertcharernrit@gmail.com>
2019-01-10 15:06:33 +0700

    Change to example code

--- webhooks.php
+++ webhooks.php
@@ -1,24 +1,36 @@
 <?php
-	require "vendor/autoload.php";
-
-	$access_token = '2lCdm/gOh0aY91WBYgSIElqBhaBdO5JQ4TGsEaMAXg1dIK2+JNajDLdHslTpoVYpNsEcAibd6dS9qCVYgsKSNAIlb0SpjOx/il1l/5ieloAM/ornHV78rqCeLnaG2tgyXyOjk43pbpeJu9IhA3ucawdB04t89/1O/w1cDnyilFU=';
-	$channelSecret = 'e73404a79308ea3273455ad514fa432b';
-
-
-	$content = file_get_contents('php://input');
-	$arrayJson = json_decode($content, true);
-	$arrayHeader = array();
-	$arrayHeader[] = "Content-Type: application/json";
-	$arrayHeader[] = "Authorization: Bearer {$accessToken}";
-
-	$idPush = $arrayJson['events'][0]['source']['userId'];
-
-	$message = $arrayJson['events'][0]['message']['text'];
-
-	$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-	$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-	$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
-	$response = $bot->pushMessage($idPush, $textMessageBuilder);
-
-	echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+   $accessToken = "2lCdm/gOh0aY91WBYgSIElqBhaBdO5JQ4TGsEaMAXg1dIK2+JNajDLdHslTpoVYpNsEcAibd6dS9qCVYgsKSNAIlb0SpjOx/il1l/5ieloAM/ornHV78rqCeLnaG2tgyXyOjk43pbpeJu9IhA3ucawdB04t89/1O/w1cDnyilFU=";
+   $content = file_get_contents('php://input');
+   $arrayJson = json_decode($content, true);
+   $arrayHeader = array();
+   $arrayHeader[] = "Content-Type: application/json";
+   $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+   //รับข้อความจากผู้ใช้
+   $message = $arrayJson['events'][0]['message']['text'];
+   //รับ id ของผู้ใช้
+   $id = $arrayJson['events'][0]['source']['userId'];
+   #ตัวอย่าง Message Type "Text + Sticker"
+   if($message == "a"){
+      $arrayPostData['to'] = $id;
+      $arrayPostData['messages'][0]['type'] = "text";
+      $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+      $arrayPostData['messages'][1]['type'] = "sticker";
+      $arrayPostData['messages'][1]['packageId'] = "2";
+      $arrayPostData['messages'][1]['stickerId'] = "34";
+      pushMsg($arrayHeader,$arrayPostData);
+   }
+   function pushMsg($arrayHeader,$arrayPostData){
+      $strUrl = "https://api.line.me/v2/bot/message/push";
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL,$strUrl);
+      curl_setopt($ch, CURLOPT_HEADER, false);
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      $result = curl_exec($ch);
+      curl_close ($ch);
+   }
+   exit;
 ?>
